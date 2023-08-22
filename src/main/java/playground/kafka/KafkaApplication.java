b7a9fe58311666962c754a21890db4f103d51205
GI YONG GIM <gygimdev@GIs-MacBook-Pro.local>
2023-08-22 09:01:59 +0900

    민감정보제거

--- src/main/java/playground/kafka/KafkaApplication.java
+++ src/main/java/playground/kafka/KafkaApplication.java
@@ -27,7 +27,6 @@ public class KafkaApplication {
 		// properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 		properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
 		properties.setProperty("security.protocol", "SASL_SSL");
-		properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"10coOvudYtXZyestPxE8W1\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIxMGNvT3Z1ZFl0WFp5ZXN0UHhFOFcxIiwib3JnYW5pemF0aW9uSWQiOjc1NDY3LCJ1c2VySWQiOjg3ODAyLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI4ODk3NzI5Ni00NzdiLTRjZTEtYTljMy1iMDgxZDdhNjk1ZjIifX0.E6Hcz7V1axQwPsSEUqHWjn1g19Tm3zR7x7JY16-Pd30\";");
 		properties.setProperty("sasl.mechanism", "PLAIN");
 
 		// b. 프로듀서 설정
